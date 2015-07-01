6c93f5781e071609360225d21e83e86d7b211ae5
Peter Humburg <peter.humburg@gmail.com>
2015-07-01 00:00:00 +0000

    version 1.0-7

--- inst/doc/tileHMM.R
+++ inst/doc/tileHMM.R
@@ -1,5 +1,5 @@
 ### R code from vignette source 'tileHMM.Rnw'
-### Encoding: UTF-8
+### Encoding: ISO8859-1
 
 ###################################################
 ### code chunk number 1: tileHMM.Rnw:38-41
